a33d5d2301031c93450534d8c7c597381d576ec9
Syrus Akbary <me@syrusakbary.com>
2018-03-13 23:08:17 -0700

    Improved google analytics support

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-jsx`,
-    `@jacobmischka/gatsby-plugin-react-svg`
+    `@jacobmischka/gatsby-plugin-react-svg`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-12613282-7"
+        // Puts tracking script in the head instead of the body
+        // head: false,
+        // Setting this parameter is optional
+        // anonymize: true,
+        // Setting this parameter is also optional
+        // respectDNT: true,
+      }
+    }
   ]
 };
