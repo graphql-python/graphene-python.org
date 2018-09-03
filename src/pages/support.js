e7ecc6084a8182387ebe93868cf7b9662e6d47ff
Syrus Akbary <me@syrusakbary.com>
2018-09-03 20:19:14 +0200

    Added support page

---
+++ src/pages/support.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Link from "gatsby-link";
+import {
+  FaBriefcase,
+  FaMapMarker,
+  FaGlobe,
+  FaGithub,
+  FaLink,
+  FaTwitter
+} from "react-icons/fa";
+import paypal from "./support/paypal.png";
+
+const Support = () => {
+  return (
+    <div className="small-container">
+      <h1>Support Graphene Development</h1>
+      <p>
+        Graphene is an MIT licensed open source project and completely free to
+        use. However, the amount of effort needed to maintain and develop new
+        features for the project is not sustainable without proper financial
+        backing. You can support Graphene development via the following methods:
+      </p>
+      <div>
+        <h2>One-time donations</h2>
+        <p>We accept donations through these channels:</p>
+        <a href="https://www.paypal.me/syrusakbary" target="_blank">
+          <img src={paypal} width="100" />
+        </a>
+      </div>
+      <div>
+        <h2>Recurring Pledges</h2>
+        <p>
+          Recurring pledges come with exclusive perks, e.g. having your name
+          listed in the Vue GitHub repository, or have your company logo placed
+          on this website.
+        </p>
+        <ul>
+          <li>
+            <a href="https://www.patreon.com/graphene" target="_blank">
+              Become a backer or sponsor via Patreon
+            </a>{" "}
+            (goes directly to support Syrus Akbary's full-time work on Graphene)
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>Current Premium Sponsors: </h2>
+      </div>
+      <p>
+        Signing up for a paid plan will:
+        <ul>
+          <li>
+            Directly contribute to faster releases, more features, and higher
+            quality software.
+          </li>
+          <li>
+            Allow more time to be invested in documentation, issue triage, and
+            community support.
+          </li>
+          <li>Safeguard the future development of Graphene.</li>
+        </ul>
+      </p>
+
+      <p>
+        If you run a business and are using Graphene in a revenue-generating
+        product, it makes business sense to sponsor Graphene development:{" "}
+        <b>
+          it ensures the project that your product relies on stays healthy and
+          actively maintained
+        </b>. It can also help your exposure in the Graphene community and makes
+        it easier to attract Graphene developers.
+      </p>
+      <p>
+        If you are an individual user and have enjoyed the productivity of using
+        Graphene, consider donating as a sign of appreciation - like buying me
+        coffee once in a while 😊
+      </p>
+    </div>
+  );
+};
+
+export default Support;
